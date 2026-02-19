4c0faf20f3cf4b56b519145b102f3debec82f2c5
James Harper <james.harper@woxford.com>
2026-02-18 23:14:13 -0600

    fi

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
-use chrono::{TimeDelta, prelude::*};
+use chrono::prelude::*;
+use crossterm::{
+    ExecutableCommand, QueueableCommand,
+    cursor::{self, MoveTo},
+    style::{self, Print},
+    terminal::{self, WindowSize, disable_raw_mode, enable_raw_mode, size, window_size},
+};
 use std::{
     io::{self, Write},
     thread::{self, sleep},
     time::Duration,
 };
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    enable_raw_mode()?;
+
+    let mut stdout = io::stdout();
+
+    stdout.execute(cursor::Hide)?;
+
     loop {
         print!("\x1B[2J\x1B[H");
         io::stdout().flush().unwrap();
 
         let now_local: DateTime<Local> = Local::now();
-        println!("The time is: {} ", now_local.format("%H:%M:%S"));
+        println!("{} ", now_local.format("%H:%M:%S"));
+
+        let curr_window_size: WindowSize = window_size()?;
+
+        println!("{}", curr_window_size.rows);
 
         let next_minute_start = now_local
             .with_second(0)
